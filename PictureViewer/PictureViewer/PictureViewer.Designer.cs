cd3569ddf953ad4b910537885e8fbc66a99c2a6d
Quoc Minh Bui <bqmxnh@gmail.com>
2024-03-14 16:20:53 +0700

    Add features to Picture Viewer

--- PictureViewer/PictureViewer/PictureViewer.Designer.cs
+++ PictureViewer/PictureViewer/PictureViewer.Designer.cs
@@ -29,41 +29,66 @@ namespace PictureViewer
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PictureViewer));
             this.nutChoose = new System.Windows.Forms.Button();
-            this.PictureBox = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.flowLayoutPanelThumbnails = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+            this.flowLayoutPanelThumbnails.SuspendLayout();
             this.SuspendLayout();
             // 
             // nutChoose
             // 
-            this.nutChoose.Location = new System.Drawing.Point(12, 12);
+            this.nutChoose.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nutChoose.Location = new System.Drawing.Point(12, 63);
             this.nutChoose.Name = "nutChoose";
-            this.nutChoose.Size = new System.Drawing.Size(116, 53);
+            this.nutChoose.Size = new System.Drawing.Size(116, 186);
             this.nutChoose.TabIndex = 0;
             this.nutChoose.Text = "Choose File to Open";
             this.nutChoose.UseVisualStyleBackColor = true;
             this.nutChoose.Click += new System.EventHandler(this.button1_Click);
             // 
-            // PictureBox
+            // pictureBoxMain
             // 
-            this.PictureBox.Location = new System.Drawing.Point(128, 71);
-            this.PictureBox.Name = "PictureBox";
-            this.PictureBox.Size = new System.Drawing.Size(651, 361);
-            this.PictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.PictureBox.TabIndex = 1;
-            this.PictureBox.TabStop = false;
+            this.pictureBoxMain.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxMain.Location = new System.Drawing.Point(12, 326);
+            this.pictureBoxMain.Name = "pictureBoxMain";
+            this.pictureBoxMain.Size = new System.Drawing.Size(746, 333);
+            this.pictureBoxMain.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxMain.TabIndex = 1;
+            this.pictureBoxMain.TabStop = false;
             // 
-            // Form1
+            // flowLayoutPanelThumbnails
+            // 
+            this.flowLayoutPanelThumbnails.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanelThumbnails.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanelThumbnails.Location = new System.Drawing.Point(134, 12);
+            this.flowLayoutPanelThumbnails.Name = "flowLayoutPanelThumbnails";
+            this.flowLayoutPanelThumbnails.Size = new System.Drawing.Size(624, 308);
+            this.flowLayoutPanelThumbnails.TabIndex = 2;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(36, 12);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // PictureViewer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.PictureBox);
+            this.ClientSize = new System.Drawing.Size(770, 671);
+            this.Controls.Add(this.flowLayoutPanelThumbnails);
+            this.Controls.Add(this.pictureBoxMain);
             this.Controls.Add(this.nutChoose);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PictureViewer";
+            this.Text = "Picture Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+            this.flowLayoutPanelThumbnails.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -71,7 +96,9 @@ namespace PictureViewer
         #endregion
 
         private System.Windows.Forms.Button nutChoose;
-        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.PictureBox pictureBoxMain;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelThumbnails;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
     }
 }
 
--- PictureViewer/PictureViewer/PictureViewer.cs
+++ PictureViewer/PictureViewer/PictureViewer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,19 +13,82 @@ namespace PictureViewer
 {
     public partial class PictureViewer : Form
     {
+        private List<string> imageFiles; // Danh sach cac tep hinh anh
+        private int currentImageIndex = 0; // Chi so cua cac hinh anh hien tai
+
         public PictureViewer()
         {
             InitializeComponent();
         }
 
+        // Khi nguoi dung nhan nut Choose File to Open
         private void button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
-            if (ofd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            ofd.Multiselect = true;// Cho phep chon nhieu tep
+            ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif;*.bmp";// Dinh dang cac tep hinh anh
+            if (ofd.ShowDialog() == DialogResult.OK)
             {
-                Image img = Image.FromFile(ofd.FileName);
-                PictureBox.Image = img;
+                imageFiles = ofd.FileNames.ToList();// Hien thi danh sach hinh anh
+                DisplayThumbnails();// Hien thi cac hinh anh da chon
+                DisplayCurrentImage();// Hien thi hinh anh hien tai
             }
+        }
+        // Khu vuc hien thi danh sach hinh anh
+        private void DisplayThumbnails()
+        {
+            flowLayoutPanelThumbnails.Controls.Clear();// Xoa cac hinh anh cu
+
+            foreach (string imagePath in imageFiles)
+            {
+                PictureBox pictureBox = new PictureBox();// Tao cac PictureBox moi
+                pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;// Che do hien thi: StrechImage
+                pictureBox.Width = 100; // Rong PictureBox
+                pictureBox.Height = 100; // Cao PictureBox
+                pictureBox.Margin = new Padding(5); // Khoang cach giua cac hinh anh
+                pictureBox.Image = Image.FromFile(imagePath); // Dat hinh anh cho PictureBox
+                pictureBox.Click += Thumbnail_Click;// Them su kien Click cho PictureBox 
+                flowLayoutPanelThumbnails.Controls.Add(pictureBox); // Them hinh anh
+            }
+        }
+
+        private void DisplayCurrentImage()
+        {
+            if (imageFiles != null && imageFiles.Count > 0)
+            {
+                if (currentImageIndex < 0)
+                    currentImageIndex = 0;// Gioi han duoi
+                else if (currentImageIndex >= imageFiles.Count)
+                    currentImageIndex = imageFiles.Count - 1; // Gioi han tren
+
+                string currentImagePath = imageFiles[currentImageIndex];// Lay duong dan hinh anh
+                pictureBoxMain.Image = Image.FromFile(currentImagePath);// Dat hinh anh vao trong PictureBoxMain
+            }
+        }
+        // Khi nguoi dung nhan vao mot hinh anh thi nho
+        private void Thumbnail_Click(object sender, EventArgs e)
+        {
+            PictureBox clickedPictureBox = sender as PictureBox; // Lay hinh anh da duoc click ra
+            int index = flowLayoutPanelThumbnails.Controls.IndexOf(clickedPictureBox);// Xac dinh chi so hinh anh
+            currentImageIndex = index;// Xac dinh chi so hinh anh hien tai
+            DisplayCurrentImage(); // Hien thi hinh anh
+        }
+        // Khi nguoi dung nhan left-right arrows 
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Left)
+            {
+                currentImageIndex--;// Chuyen ve hinh anh truoc do
+                DisplayCurrentImage();// Hien thi hinh anh
+                return true;
+            }
+            else if (keyData == Keys.Right)
+            {
+                currentImageIndex++;// Chuyen den hinh anh tiep theo
+                DisplayCurrentImage();// Hien thi hinh anh
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
         }
     }
 }
