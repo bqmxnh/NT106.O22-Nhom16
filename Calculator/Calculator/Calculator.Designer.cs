ace831837f07f810616f14df8caf28c96888d8c2
kieungann <kieungan091122@gmail.com>
2024-03-14 13:46:58 +0700

    Add features to Calculator

--- Calculator/Calculator/Calculator.Designer.cs
+++ Calculator/Calculator/Calculator.Designer.cs
@@ -54,228 +54,248 @@
             // 
             richTextBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             richTextBox1.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            richTextBox1.Location = new Point(23, 59);
-            richTextBox1.Margin = new Padding(3, 4, 3, 4);
+            richTextBox1.Location = new Point(18, 47);
+            richTextBox1.Margin = new Padding(2, 3, 2, 3);
             richTextBox1.Name = "richTextBox1";
             richTextBox1.ScrollBars = RichTextBoxScrollBars.None;
-            richTextBox1.Size = new Size(414, 90);
+            richTextBox1.Size = new Size(332, 73);
             richTextBox1.TabIndex = 0;
             richTextBox1.Text = "";
+            richTextBox1.KeyDown += richTextBox1_keydown;
             // 
             // button1
             // 
             button1.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.Location = new Point(22, 176);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(18, 141);
+            button1.Margin = new Padding(2, 3, 2, 3);
             button1.Name = "button1";
-            button1.Size = new Size(69, 82);
+            button1.Size = new Size(55, 66);
             button1.TabIndex = 1;
             button1.Text = "7";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button_Click;
             // 
             // button2
             // 
             button2.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button2.Location = new Point(111, 176);
-            button2.Margin = new Padding(3, 4, 3, 4);
+            button2.Location = new Point(89, 141);
+            button2.Margin = new Padding(2, 3, 2, 3);
             button2.Name = "button2";
-            button2.Size = new Size(69, 82);
+            button2.Size = new Size(55, 66);
             button2.TabIndex = 1;
             button2.Text = "8";
             button2.UseVisualStyleBackColor = true;
+            button2.Click += button_Click;
             // 
             // button3
             // 
             button3.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button3.Location = new Point(197, 176);
-            button3.Margin = new Padding(3, 4, 3, 4);
+            button3.Location = new Point(158, 141);
+            button3.Margin = new Padding(2, 3, 2, 3);
             button3.Name = "button3";
-            button3.Size = new Size(69, 82);
+            button3.Size = new Size(55, 66);
             button3.TabIndex = 1;
             button3.Text = "9";
             button3.UseVisualStyleBackColor = true;
+            button3.Click += button_Click;
             // 
             // button4
             // 
             button4.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button4.Location = new Point(282, 176);
-            button4.Margin = new Padding(3, 4, 3, 4);
+            button4.Location = new Point(226, 141);
+            button4.Margin = new Padding(2, 3, 2, 3);
             button4.Name = "button4";
-            button4.Size = new Size(69, 82);
+            button4.Size = new Size(55, 66);
             button4.TabIndex = 1;
             button4.Text = "C";
             button4.UseVisualStyleBackColor = true;
+            button4.Click += button_clear;
             // 
             // button5
             // 
             button5.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button5.Location = new Point(368, 176);
-            button5.Margin = new Padding(3, 4, 3, 4);
+            button5.Location = new Point(294, 141);
+            button5.Margin = new Padding(2, 3, 2, 3);
             button5.Name = "button5";
-            button5.Size = new Size(69, 82);
+            button5.Size = new Size(55, 66);
             button5.TabIndex = 1;
             button5.Text = "CE";
             button5.UseVisualStyleBackColor = true;
+            button5.Click += button_clearentry;
             // 
             // button6
             // 
             button6.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button6.Location = new Point(22, 278);
-            button6.Margin = new Padding(3, 4, 3, 4);
+            button6.Location = new Point(18, 222);
+            button6.Margin = new Padding(2, 3, 2, 3);
             button6.Name = "button6";
-            button6.Size = new Size(69, 82);
+            button6.Size = new Size(55, 66);
             button6.TabIndex = 1;
             button6.Text = "4";
             button6.UseVisualStyleBackColor = true;
+            button6.Click += button_Click;
             // 
             // button7
             // 
             button7.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button7.Location = new Point(111, 278);
-            button7.Margin = new Padding(3, 4, 3, 4);
+            button7.Location = new Point(89, 222);
+            button7.Margin = new Padding(2, 3, 2, 3);
             button7.Name = "button7";
-            button7.Size = new Size(69, 82);
+            button7.Size = new Size(55, 66);
             button7.TabIndex = 1;
             button7.Text = "5";
             button7.UseVisualStyleBackColor = true;
+            button7.Click += button_Click;
             // 
             // button8
             // 
             button8.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button8.Location = new Point(197, 278);
-            button8.Margin = new Padding(3, 4, 3, 4);
+            button8.Location = new Point(158, 222);
+            button8.Margin = new Padding(2, 3, 2, 3);
             button8.Name = "button8";
-            button8.Size = new Size(69, 82);
+            button8.Size = new Size(55, 66);
             button8.TabIndex = 1;
             button8.Text = "6";
             button8.UseVisualStyleBackColor = true;
+            button8.Click += button_Click;
             // 
             // button9
             // 
             button9.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button9.Location = new Point(282, 278);
-            button9.Margin = new Padding(3, 4, 3, 4);
+            button9.Location = new Point(226, 222);
+            button9.Margin = new Padding(2, 3, 2, 3);
             button9.Name = "button9";
-            button9.Size = new Size(69, 82);
+            button9.Size = new Size(55, 66);
             button9.TabIndex = 1;
             button9.Text = "+";
             button9.UseVisualStyleBackColor = true;
+            button9.Click += button_Click;
             // 
             // button10
             // 
             button10.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button10.Location = new Point(368, 278);
-            button10.Margin = new Padding(3, 4, 3, 4);
+            button10.Location = new Point(294, 222);
+            button10.Margin = new Padding(2, 3, 2, 3);
             button10.Name = "button10";
-            button10.Size = new Size(69, 82);
+            button10.Size = new Size(55, 66);
             button10.TabIndex = 1;
             button10.Text = "/";
             button10.UseVisualStyleBackColor = true;
+            button10.Click += button_Click;
             // 
             // button11
             // 
             button11.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button11.Location = new Point(368, 380);
-            button11.Margin = new Padding(3, 4, 3, 4);
+            button11.Location = new Point(294, 304);
+            button11.Margin = new Padding(2, 3, 2, 3);
             button11.Name = "button11";
-            button11.Size = new Size(69, 82);
+            button11.Size = new Size(55, 66);
             button11.TabIndex = 1;
             button11.Text = "-";
             button11.UseVisualStyleBackColor = true;
+            button11.Click += button_Click;
             // 
             // button12
             // 
             button12.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button12.Location = new Point(281, 380);
-            button12.Margin = new Padding(3, 4, 3, 4);
+            button12.Location = new Point(225, 304);
+            button12.Margin = new Padding(2, 3, 2, 3);
             button12.Name = "button12";
-            button12.Size = new Size(69, 82);
+            button12.Size = new Size(55, 66);
             button12.TabIndex = 1;
             button12.Text = "*";
             button12.UseVisualStyleBackColor = true;
+            button12.Click += button_Click;
             // 
             // button13
             // 
             button13.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button13.Location = new Point(197, 380);
-            button13.Margin = new Padding(3, 4, 3, 4);
+            button13.Location = new Point(158, 304);
+            button13.Margin = new Padding(2, 3, 2, 3);
             button13.Name = "button13";
-            button13.Size = new Size(69, 82);
+            button13.Size = new Size(55, 66);
             button13.TabIndex = 1;
             button13.Text = "3";
             button13.UseVisualStyleBackColor = true;
+            button13.Click += button_Click;
             // 
             // button14
             // 
             button14.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button14.Location = new Point(111, 380);
-            button14.Margin = new Padding(3, 4, 3, 4);
+            button14.Location = new Point(89, 304);
+            button14.Margin = new Padding(2, 3, 2, 3);
             button14.Name = "button14";
-            button14.Size = new Size(69, 82);
+            button14.Size = new Size(55, 66);
             button14.TabIndex = 1;
             button14.Text = "2";
             button14.UseVisualStyleBackColor = true;
+            button14.Click += button_Click;
             // 
             // button15
             // 
             button15.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button15.Location = new Point(22, 380);
-            button15.Margin = new Padding(3, 4, 3, 4);
+            button15.Location = new Point(18, 304);
+            button15.Margin = new Padding(2, 3, 2, 3);
             button15.Name = "button15";
-            button15.Size = new Size(69, 82);
+            button15.Size = new Size(55, 66);
             button15.TabIndex = 1;
             button15.Text = "1";
             button15.UseVisualStyleBackColor = true;
+            button15.Click += button_Click;
             // 
             // button16
             // 
             button16.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button16.Location = new Point(22, 482);
-            button16.Margin = new Padding(3, 4, 3, 4);
+            button16.Location = new Point(18, 386);
+            button16.Margin = new Padding(2, 3, 2, 3);
             button16.Name = "button16";
-            button16.Size = new Size(158, 82);
+            button16.Size = new Size(126, 66);
             button16.TabIndex = 1;
             button16.Text = "0";
             button16.UseVisualStyleBackColor = true;
+            button16.Click += button_Click;
             // 
             // button17
             // 
             button17.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button17.Location = new Point(368, 482);
-            button17.Margin = new Padding(3, 4, 3, 4);
+            button17.Location = new Point(294, 386);
+            button17.Margin = new Padding(2, 3, 2, 3);
             button17.Name = "button17";
-            button17.Size = new Size(69, 82);
+            button17.Size = new Size(55, 66);
             button17.TabIndex = 1;
             button17.Text = "=";
             button17.UseVisualStyleBackColor = true;
+            button17.Click += button_equal;
             // 
             // button18
             // 
             button18.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button18.Location = new Point(281, 482);
-            button18.Margin = new Padding(3, 4, 3, 4);
+            button18.Location = new Point(225, 386);
+            button18.Margin = new Padding(2, 3, 2, 3);
             button18.Name = "button18";
-            button18.Size = new Size(69, 82);
+            button18.Size = new Size(55, 66);
             button18.TabIndex = 1;
             button18.Text = "%";
             button18.UseVisualStyleBackColor = true;
+            button18.Click += button_Click;
             // 
             // button19
             // 
             button19.Font = new Font("Microsoft Sans Serif", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button19.Location = new Point(197, 482);
-            button19.Margin = new Padding(3, 4, 3, 4);
+            button19.Location = new Point(158, 386);
+            button19.Margin = new Padding(2, 3, 2, 3);
             button19.Name = "button19";
-            button19.Size = new Size(69, 82);
+            button19.Size = new Size(55, 66);
             button19.TabIndex = 1;
             button19.Text = ".";
             button19.UseVisualStyleBackColor = true;
+            button19.Click += button_Click;
             // 
             // Calculator
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(457, 599);
+            ClientSize = new Size(366, 479);
             Controls.Add(button17);
             Controls.Add(button18);
             Controls.Add(button19);
@@ -296,6 +316,7 @@
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(richTextBox1);
+            Margin = new Padding(2);
             Name = "Calculator";
             Text = "Calculator";
             ResumeLayout(false);
--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -1,3 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 namespace Calculator
 {
     public partial class Calculator : Form
@@ -5,6 +14,60 @@ namespace Calculator
         public Calculator()
         {
             InitializeComponent();
+        }
+        private int flag = 0;
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (flag == 1)
+            {
+                richTextBox1.Text = "";
+                flag = 0;
+            }
+            richTextBox1.Text += (sender as Button).Text;
+        }
+
+        private void button_clear(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+        }
+
+        private void button_clearentry(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length > 0)
+            {
+                richTextBox1.Text = richTextBox1.Text.Remove(richTextBox1.Text.Length - 1, 1);
+            }
+        }
+
+        private void button_equal(object sender, EventArgs e)
+        {
+            try
+            {
+                richTextBox1.Text = new DataTable().Compute(richTextBox1.Text, null).ToString();
+                flag = 1;
+            }
+            catch (Exception)
+            {
+                richTextBox1.Text = "Error";
+                flag = 1;
+            }
+        }
+
+        private void richTextBox1_keydown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                try
+                {
+                    richTextBox1.Text = new DataTable().Compute(richTextBox1.Text, null).ToString();
+                    flag = 1;
+                }
+                catch (Exception)
+                {
+                    richTextBox1.Text = "Error";
+                    flag = 1;
+                }
+            }
         }
     }
 }
