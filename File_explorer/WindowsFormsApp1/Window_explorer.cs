071789bccf3d9cf50eb40ef5b4ebe133c6ec3546
Minh Phat <mphat2824@gmail.com>
2024-03-14 22:51:29 +0700

    Change file explorer solution

---
+++ File_explorer/WindowsFormsApp1/Window_explorer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Window_explorer : Form
+    {
+        public Window_explorer()
+        {
+            InitializeComponent();
+        }
+
+        private void opentbtn_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog() { Description = "Choose your file" })
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    webBrowser1.Url = new Uri(fbd.SelectedPath);
+                    pathtxt.Text = fbd.SelectedPath;
+                }
+                else                   
+                    MessageBox.Show("No file selected");
+            }
+        }
+
+        private void forwardbtn_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoForward)
+                webBrowser1.GoForward();
+        }
+
+        private void backbtn_Click(object sender, EventArgs e)
+        {
+            if (webBrowser1.CanGoBack)
+                webBrowser1.GoBack();
+        }
+
+        private void txtboxkeydown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    webBrowser1.Url = new Uri(pathtxt.Text);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Invalid URL");
+            }
+        }
+    }
+}
